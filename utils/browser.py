4ec5b0d528b458ae12a705db628f5f3b8ae2ca5e
CadeM2021 <mcdonaghcade112@gmail.com>
2025-07-23 11:59:22 -0700

    Update browser.py

--- utils/browser.py
+++ utils/browser.py
@@ -1,1 +1,20 @@
+# utils/browser.py
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
 
+def init_driver(headless=True):
+    """Automatically manages ChromeDriver installation"""
+    options = Options()
+    if headless:
+        options.add_argument("--headless=new")
+    options.add_argument("--disable-blink-features=AutomationControlled")
+    
+    # Auto-download and manage ChromeDriver
+    service = Service(ChromeDriverManager().install())
+    driver = webdriver.Chrome(service=service, options=options)
+    
+    # Configure timeouts
+    driver.set_page_load_timeout(30)
+    return driver
